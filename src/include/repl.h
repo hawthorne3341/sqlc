7e48239e89155647a081ed3d88d6e0141dccbe7a
Luke Hawthorne <hawthorne3341@gmail.com>
2022-10-27 21:47:16 -0700

    Part 3

--- src/include/repl.h
+++ src/include/repl.h
@@ -1,5 +1,13 @@
+#include <stdint.h>
 #ifndef REPL_H
 #define REPL_H
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+#define TABLE_MAX_PAGES 100
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 
   typedef enum {
     META_COMMAND_SUCCESS,
@@ -8,8 +16,14 @@
 
   typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
   } PrepareResult;
+
+  typedef enum {
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+  } ExecuteResult;
 
   typedef enum {
     STATEMENT_INSERT,
@@ -23,7 +37,19 @@
   } InputBuffer;
 
   typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+  } Row;
+
+  typedef struct {
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+  } Table;
+
+  typedef struct {
     StatementType type;
+    Row row_to_insert; // only used by insert statement
   } Statement;
 
   void start_repl();
