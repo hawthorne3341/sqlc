13e9f9c2290f006132ea15fa9ac7506290191cc1
Luke Hawthorne <hawthorne3341@gmail.com>
2022-10-27 21:21:48 -0700

    Part 2

--- src/include/repl.h
+++ src/include/repl.h
@@ -1,11 +1,30 @@
 #ifndef REPL_H
 #define REPL_H
 
+  typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+  } MetaCommandResult;
+
+  typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+  } PrepareResult;
+
+  typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+  } StatementType;
+
   typedef struct {
     char* buffer;
     size_t buffer_length;
     ssize_t input_length;
   } InputBuffer;
+
+  typedef struct {
+    StatementType type;
+  } Statement;
 
   void start_repl();
 
